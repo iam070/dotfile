a77064a856f6f1b8cbc1f935db4fdfbab914063f
Lin Ming <iam070@qq.com>
2016-04-19 00:31:09 +0800

    auto download source package

--- make_llvm_clang.sh
+++ make_llvm_clang.sh
@@ -1,44 +1,64 @@
-#!/usr/bin/sh
+#!/bin/bash
 function print_usage() {
-    echo "Firstly donwload llvm, clang and compiler-rt source packages from "\
-         "llvm official website into one folder. Then run this script will "\
-         "auto extract tars and make it."
+    echo "Auto download and make the llvm with clang by specific version."
 }
 
 if [ $# != 1 ]
 then
     echo 'Please specify the version.'
-    print_usage
-    return 1
+    exit 1
 fi
 
 LLVM_DIR=llvm-src
 
+BASE_LINK="http://llvm.org/releases/$1/"
+
 LLVM_SRC=llvm-$1.src
 LLVM_SRC_TAR=$LLVM_SRC.tar.xz
+LLVM_SRC_TAR_LINK=$BASE_LINK$LLVM_SRC_TAR
 
 CLANG_SRC=cfe-$1.src
 CLANG_SRC_TAR=$CLANG_SRC.tar.xz
+CLANG_SRC_TAR_LINK=$BASE_LINK$CLANG_SRC_TAR
 
 COMP_RT_SRC=compiler-rt-$1.src
 COMP_RT_SRC_TAR=$COMP_RT_SRC.tar.xz
+COMP_RT_SRC_TAR=$BASE_LINK$COMP_RT_SRC_TAR
 
 if [ ! -f $LLVM_SRC_TAR ]; then
     echo "llvm source tar $LLVM_SRC_TAR does not exist!"
-    print_usage
-    return 1
+    echo "Begin to download $LLVM_SRC_TAR."
+    wget $LLVM_SRC_TAR_LINK
+    if [ $? != 0 ]
+    then
+        echo "Failed to download $LLVM_SRC_TAR_LINK!"
+        rm -f $LLVM_SRC_TAR
+        exit 1
+    fi
 fi
 
 if [ ! -f $CLANG_SRC_TAR ]; then
     echo "clang source tar $CLANG_SRC_TAR does not exist!"
-    print_usage
-    return 1
+    echo "Begin to download $CLANG_SRC_TAR."
+    wget $CLANG_SRC_TAR_LINK
+    if [ $? != 0 ]
+    then
+        echo "Failed to download $CLANG_SRC_TAR_LINK!"
+        rm -f $CLANG_SRC_TAR
+        exit 1
+    fi
 fi
 
 if [ ! -f $COMP_RT_SRC_TAR ]; then
     echo "compiler-rt source tar $COMP_RT_SRC_TAR does not exist!"
-    print_usage
-    return 1
+    echo "Begin to donload $COMP_RT_SRC_TAR"
+    wget $COMP_RT_SRC_TAR_LINK
+    if [ $? != 0]
+    then
+        echo "Failed to download $COMP_RT_SRC_TAR"
+        rm -f $COMP_RT_SRC_TAR
+        exit 1
+    fi
 fi
 
 tar xvf $LLVM_SRC_TAR
@@ -56,4 +76,4 @@ cd build
 cmake -G "Unix Makefiles" ..
 make
 
-return 0
+exit 0
